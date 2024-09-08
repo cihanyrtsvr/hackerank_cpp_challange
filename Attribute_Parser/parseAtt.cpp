a346c378599160050853cfe16798fe8cfc8a8d64
cihanyrtsvr <cihan.yurtsever1@gmail.com>
2024-09-08 13:23:31 +0200

    "run.bat" file is correctly edited to run the files on the documents
    The AttributeParse example created working on it..

--- Attribute_Parser/parseAtt.cpp
+++ Attribute_Parser/parseAtt.cpp
@@ -7,27 +7,64 @@ using namespace std;
 
 int main() {
     int N, Q;
+
     cin >> N >> Q;
     cin.ignore();
 
     unordered_map<string, unordered_map<string, string>> tag_list;
     vector<string> current_tag;
 
-    string line;
+    string line, query;
+
     for (int i = 0; i < N; i++) {
         getline(cin, line);
         
-        stringstream ss(line);
-        string tag, attr, value;
+        if(line.substr(0,2)=="</"){
+            current_tag.pop_back();
+        }
+        else{
 
-        ss >> tag;  // Read '<' and the tag name
-        tag = tag.substr(1);
-        cout <<"Tag: "<< tag << endl;
+            stringstream ss(line);
+            string tag, attr, value, pass, current_tag_path;
 
-        // Skip the rest of the line
-        
+            ss >> tag;  // Read '<' and the tag name
+            tag = tag.substr(1);
+
+            if(current_tag.empty()){
+                current_tag.emplace_back(tag);
+            }
+            else{
+                current_tag.emplace_back(current_tag.back() + "."+tag);
+            }
+
+            while(ss>>attr){
+                ss>>pass;
+                ss>>value;
+
+                value = value.substr(1,value.size()-3);
+                current_tag_path = current_tag.back();
+                tag_list[current_tag_path][attr] = value;
+            }
+            // Skip the rest of the line
+            
+                
+        }
     }
 
+    for(int j=0 ; j<Q; j++){
+        getline(cin,query);
+        int pose = query.find("~");
+        string tag_head = query.substr(0,pose);
+        string value_part = query.substr(pose+1);
+
+        if(tag_list.find(tag_head)== tag_list.end() ||
+        tag_list[tag_head].find(value_part) == tag_list[tag_head].end()){
+            cout<<"Not Found!"<<endl;
+        }
+        else{
+            cout<<tag_list[tag_head][value_part]<<endl;
+        }
+    }
     // ... rest of your code ...
 
     return 0;
