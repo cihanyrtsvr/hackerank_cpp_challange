7e7da303a82f2e608887d318d2420b22d898b632
cihanyrtsvr <cihan.yurtsever1@gmail.com>
2024-09-09 22:12:40 +0200

    Clang first example

--- Attribute_Parser/parseAtt.cpp
+++ Attribute_Parser/parseAtt.cpp
@@ -1,70 +1,66 @@
 #include <iostream>
 #include <sstream>
-#include <vector>
 #include <unordered_map>
+#include <vector>
+
 
 using namespace std;
 
 int main() {
-    int N, Q;
+  int N, Q;
 
-    cin >> N >> Q;
-    cin.ignore();
+  cin >> N >> Q;
+  cin.ignore();
 
-    unordered_map<string, unordered_map<string, string>> tag_list;
-    vector<string> current_tag;
+  unordered_map<string, unordered_map<string, string>> tag_list;
+  vector<string> current_tag;
 
-    string line, query;
+  string line, query;
 
-    for (int i = 0; i < N; i++) {
-        getline(cin, line);
-        
-        if(line.substr(0,2)=="</"){
-            current_tag.pop_back();
-        }
-        else{
+  for (int i = 0; i < N; i++) {
+    getline(cin, line);
 
-            stringstream ss(line);
-            string tag, attr, value, pass, current_tag_path;
+    if (line.substr(0, 2) == "</") {
+      current_tag.pop_back();
+    } else {
 
-            ss >> tag;  // Read '<' and the tag name
-            tag = tag.substr(1);
+      stringstream ss(line);
+      string tag, attr, value, pass, current_tag_path;
 
-            if(current_tag.empty()){
-                current_tag.emplace_back(tag);
-            }
-            else{
-                current_tag.emplace_back(current_tag.back() + "."+tag);
-            }
+      ss >> tag; // Read '<' and the tag name
+      tag = tag.substr(1);
 
-            while(ss>>attr){
-                ss>>pass;
-                ss>>value;
+      if (current_tag.empty()) {
+        current_tag.emplace_back(tag);
+      } else {
+        current_tag.emplace_back(current_tag.back() + "." + tag);
+      }
 
-                value = value.substr(1,value.size()-3);
-                current_tag_path = current_tag.back();
-                tag_list[current_tag_path][attr] = value;
-            }
-            // Skip the rest of the line
-            
-                
-        }
+      while (ss >> attr) {
+        ss >> pass;
+        ss >> value;
+
+        value = value.substr(1, value.size() - 3);
+        current_tag_path = current_tag.back();
+        tag_list[current_tag_path][attr] = value;
+      }
+      // Skip the rest of the line
     }
+  }
 
-    for(int j=0 ; j<Q; j++){
-        getline(cin,query);
-        int pose = query.find("~");
-        string tag_head = query.substr(0,pose);
-        string value_part = query.substr(pose+1);
+  for (int j = 0; j < Q; j++) {
+    getline(cin, query);
+    int pose = query.find("~");
+    string tag_head = query.substr(0, pose);
+    string value_part = query.substr(pose + 1);
 
-        if(tag_list.find(tag_head)== tag_list.end() ||
-        tag_list[tag_head].find(value_part) == tag_list[tag_head].end()){
-            cout<<"Not Found!"<<endl;
-        }
-        else{
-            cout<<tag_list[tag_head][value_part]<<endl;
-        }
+    if (tag_list.find(tag_head) == tag_list.end() ||
+        tag_list[tag_head].find(value_part) == tag_list[tag_head].end()) {
+      cout << "Not Found!" << endl;
+    } else {
+      cout << tag_list[tag_head][value_part] << endl;
     }
+  }
 
-    return 0;
+  return 0;
 }
--- VirtualFunction/virtualFunc.cpp
+++ VirtualFunction/virtualFunc.cpp
@@ -1,88 +1,89 @@
+#include <algorithm>
 #include <cmath>
 #include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-
-class Person{
+class Person {
 public:
-    virtual void getdata()=0;
-    virtual void putdata()=0;
+  virtual void getdata() = 0;
+  virtual void putdata() = 0;
+
 protected:
-    string name;
-    int age;
+  string name;
+  int age;
 };
 
-class Professor : public Person{
-    private: 
-    int cur_id;
-    int static tot_num_prof;
-    int publicaitons;
-public:
-    Professor (): cur_id(tot_num_prof++){};
-    void getdata(){
-        cin>>name >> age>>publicaitons;
-        cur_id++;
-    }
-
-    void putdata()override{
-        cout<< name<<" "<<age<<" "<<publicaitons<<" "<<cur_id<<endl;
-    }
-
-};
-
-class Student : public Person{
-public:
-    Student():cur_id(tot_num_std++){}
-    void getdata() override{
-        int exam;
-        cur_id++;
-        cin>>name >> age;
-        for(int i=0; i<6; i++){
-            cin>>exam;
-            marks.emplace_back(exam);
-        }
-    }
-
-    void putdata()override{
-        int sum{0};
-        for(int i:marks){sum+=i;}
-        cout<< name<<" "<<age <<" "<<sum<<" "<<cur_id<<endl;
-    }
+class Professor : public Person {
 private:
-    int cur_id;
-    int static tot_num_std;
-    vector <int> marks;
+  int cur_id;
+  int static tot_num_prof;
+  int publicaitons;
 
+public:
+  Professor() : cur_id(tot_num_prof++){};
+  void getdata() {
+    cin >> name >> age >> publicaitons;
+    cur_id++;
+  }
+
+  void putdata() override {
+    cout << name << " " << age << " " << publicaitons << " " << cur_id << endl;
+  }
+};
+
+class Student : public Person {
+public:
+  Student() : cur_id(tot_num_std++) {}
+  void getdata() override {
+    int exam;
+    cur_id++;
+    cin >> name >> age;
+    for (int i = 0; i < 6; i++) {
+      cin >> exam;
+      marks.emplace_back(exam);
+    }
+  }
+
+  void putdata() override {
+    int sum{0};
+    for (int i : marks) {
+      sum += i;
+    }
+    cout << name << " " << age << " " << sum << " " << cur_id << endl;
+  }
+
+private:
+  int cur_id;
+  int static tot_num_std;
+  vector<int> marks;
 };
 
 int Student::tot_num_std = 0;
 int Professor::tot_num_prof = 0;
-int main(){
+int main() {
 
-    int n, val;
-    cin>>n; //The number of objects that is going to be created.
-    Person *per[n];
+  int n, val;
+  cin >> n; // The number of objects that is going to be created.
+  Person *per[n];
 
-    for(int i = 0;i < n;i++){
+  for (int i = 0; i < n; i++) {
 
-        cin>>val;
-        if(val == 1){
-            // If val is 1 current object is of type Professor
-            per[i] = new Professor;
+    cin >> val;
+    if (val == 1) {
+      // If val is 1 current object is of type Professor
+      per[i] = new Professor;
 
-        }
-        else per[i] = new Student; // Else the current object is of type Student
+    } else
+      per[i] = new Student; // Else the current object is of type Student
 
-        per[i]->getdata(); // Get the data from the user.
+    per[i]->getdata(); // Get the data from the user.
+  }
+  cout << endl << "The output: " << endl;
+  for (int i = 0; i < n; i++)
+    per[i]->putdata(); // Print the required output for each object.
 
-    }
-    cout<<endl<<"The output: "<<endl;
-    for(int i=0;i<n;i++)
-        per[i]->putdata(); // Print the required output for each object.
-
-    return 0;
-
+  return 0;
 }
